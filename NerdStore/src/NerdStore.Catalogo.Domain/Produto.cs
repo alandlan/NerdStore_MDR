00803fbc5bb398edd9bfa1b864d455fe2320354d
Alan Vieira Martins <alanhorcrux@gmail.com>
2020-05-27 19:37:39 -0300

    Criado classe Entity

---
+++ NerdStore/src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class Categoria
+    {
+
+    }}
